56c5f107f698249f8392a12003ea139b7fa379a7
rong-duo <zytchar@163.com>
2022-07-16 22:44:56 +0800

    建表

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -3,7 +3,7 @@ create table if not exists file_meta(
     name varchar(50) not null,
     path varchar(100) not null,
     is_directory boolean not null,
-    size bigint not null,
+    size bigint,
     last_modified timestamp not null,
     pinyin varchar(200),
     pinyin_first varchar(50)
