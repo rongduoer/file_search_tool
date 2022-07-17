3599a6bb9e630381d5aa7586c721a776a9bf21e9
rong-duo <zytchar@163.com>
2022-07-17 18:11:21 +0800

    文件搜索类

--- src/main/java/task/FileSearch.java
+++ src/main/java/task/FileSearch.java
@@ -31,40 +31,40 @@ public class FileSearch {
         ResultSet rs = null;
         try {
             connection = DBUtil.getConnection();
-            //先根据用户选择的文件夹dir查询内容
-            //该文件夹及其子文件夹下的所有内容
-            String sql = "select name,path,is_directory,size,last_modified from file_meta " +
-                    "where (path = ? or path like ?)";
-            if (content != null && content.trim().length() != 0){
+            // 先根据用户选择的文件夹dir查询内容
+            String sql = "select name,path,size,is_directory,last_modified from file_meta " +
+                    " where (path = ? or path like ?)";
+            if (content != null && content.trim().length() != 0) {
+                // 此时用户搜索框中的内容不为空,此处支持文件全名称，拼音全名称，以及拼音首字母的模糊查询
                 sql += " and (name like ? or pinyin like ? or pinyin_first like ?)";
             }
             ps = connection.prepareStatement(sql);
             ps.setString(1,dir);
             ps.setString(2,dir + File.separator + "%");
-            if (content != null && content.trim().length() != 0){
+            // 根据搜索框的内容查询数据库，都是模糊匹配
+            if (content != null && content.trim().length() != 0) {
                 ps.setString(3,"%" + content + "%");
                 ps.setString(4,"%" + content + "%");
                 ps.setString(5,"%" + content + "%");
             }
-            System.out.println("正在检索信息，SQL为 : " + sql);
             rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 FileMeta meta = new FileMeta();
                 meta.setName(rs.getString("name"));
                 meta.setPath(rs.getString("path"));
                 meta.setIsDirectory(rs.getBoolean("is_directory"));
-                if (meta.getIsDirectory()){
+                if (!meta.getIsDirectory()) {
+                    // 是文件，保存大小
                     meta.setSize(rs.getLong("size"));
                 }
                 meta.setLastModified(new Date(rs.getTimestamp("last_modified").getTime()));
-                System.out.println("检索到文件信息 : name = " + meta.getName() + ",path " + meta.getPath());
                 result.add(meta);
             }
-        } catch (SQLException e){
-            System.out.println("从数据库中搜索用户查找内容时出错，请检查SQL语句");
+        }catch (SQLException e) {
+            System.err.println("从数据库中搜索用户查找内容时出错，请检查SQL语句");
             e.printStackTrace();
-        } finally {
-            DBUtil.close(connection,ps,rs);
+        }finally {
+            DBUtil.close(ps,rs);
         }
         return result;
     }
