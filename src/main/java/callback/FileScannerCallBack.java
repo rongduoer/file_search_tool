20d114fcf450422d2934741e55f0aca13540fca8
rong-duo <zytchar@163.com>
2022-07-17 18:10:47 +0800

    回调接口

--- src/main/java/callback/FileScannerCallBack.java
+++ src/main/java/callback/FileScannerCallBack.java
@@ -10,5 +10,5 @@ public interface FileScannerCallBack {
      * 文件扫描的回调接口，扫描文件时由具体的子类决定将当前文件目录下的信息持久化到哪个终端，可以是数据库或者网络等
      * @param dir
      */
-    void callBack(File dir);
+    void callback(File dir);
 }
